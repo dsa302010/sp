b8bfd6b42c0c6a9bbb50cb0a1c2fcf35705526ab
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2024-07-06 23:53:27 +0800

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -62,22 +62,22 @@ def get_jerk_factor(personality=custom.LongitudinalPersonalitySP.standard):
   elif personality==custom.LongitudinalPersonalitySP.standard:
     return 1.0
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 0.6
+    return 1.0
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 0.4
+    return 0.8
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
 
 def get_T_FOLLOW(personality=custom.LongitudinalPersonalitySP.standard):
   if personality==custom.LongitudinalPersonalitySP.relaxed:
-    return 1.85
+    return 1.95
   elif personality==custom.LongitudinalPersonalitySP.standard:
-    return 1.5
+    return 1.8
   elif personality==custom.LongitudinalPersonalitySP.moderate:
-    return 1.3
+    return 1.5
   elif personality==custom.LongitudinalPersonalitySP.aggressive:
-    return 1.0
+    return 1.2
   else:
     raise NotImplementedError("Longitudinal personality not supported")
 
