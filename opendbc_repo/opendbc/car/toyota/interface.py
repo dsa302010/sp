41448f5b2a51644cca128d3ecd47b69deb816621
dsa302010 <164147796+dsa302010@users.noreply.github.com>
2025-02-11 22:05:05 +0800

    Update interface.py

--- opendbc_repo/opendbc/car/toyota/interface.py
+++ opendbc_repo/opendbc/car/toyota/interface.py
@@ -148,11 +148,11 @@ class CarInterface(CarInterfaceBase):
       if sp_tss2_long_tune:
         ret.vEgoStopping = 0.11
         ret.vEgoStarting = 0.20
-        ret.stoppingDecelRate = 0.017  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.03  # reach stopping target smoothly
       else:
         ret.vEgoStopping = 0.11
         ret.vEgoStarting = 0.25
-        ret.stoppingDecelRate = 0.016  # reach stopping target smoothly
+        ret.stoppingDecelRate = 0.03  # reach stopping target smoothly
 
       # Hybrids have much quicker longitudinal actuator response
       if ret.flags & ToyotaFlags.HYBRID.value:
